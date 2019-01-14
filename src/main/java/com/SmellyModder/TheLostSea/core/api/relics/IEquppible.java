9e73ff875a152df9789c9a4e55c7e1e41aeaa03f
SmellyModder <smellysocks543@gmail.com>
2019-01-14 17:56:40 -0500

    Fixed up

--- src/main/java/com/SmellyModder/TheLostSea/core/api/relics/IEquppible.java
+++ src/main/java/com/SmellyModder/TheLostSea/core/api/relics/IEquppible.java
@@ -5,15 +5,33 @@ import net.minecraft.item.ItemStack;
 
 public interface IEquppible {
 	
+	/*
+	 * Checks for if it's worn and can perform some action inside the method
+	 */
 	public default void onWearingTick(ItemStack itemstack, EntityLivingBase player) {}
 	
+	/*
+	 * Triggers whenever the item is equiped and can perform some action inside the method
+	 */
 	public default void onEquipped(ItemStack itemstack, EntityLivingBase player) {}
 	
+	/*
+	 * Triggers whenever the item is unequiped and can perform some action inside the method
+	 */
 	public default void onUnequipped(ItemStack itemstack, EntityLivingBase player) {}
 	
+	/*
+	 * Set's the item to be cursed or not
+	 */
 	public default boolean isCursed(ItemStack itemstack) {return false;}
 	
+	/*
+	 * Set's/Gets the item's element type
+	 */
 	public default ElementType getElementType(ItemStack stack) {return ElementType.NORMAL;}
 	
+	/*
+	 * Set's/Gets the item's type of equippable that it is; ex. EquippableType.RELIC
+	 */
 	public EquippableType getEquippableType(ItemStack stack);
 }
